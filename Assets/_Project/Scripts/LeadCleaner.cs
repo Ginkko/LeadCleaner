5ba7f7794209064c77f30d4355d5fafe8e7c3c45
Alec Arme <alec.arme@gmail.com>
2018-12-11 18:57:48 -0800

    load json from disk

---
+++ Assets/_Project/Scripts/LeadCleaner.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using NUnit.Framework;
+using UnityEngine;
+
+public class Lead
+{
+    public string Id;
+    public string Email;
+    public string FirstName;
+    public string LastName;
+    public string Address;
+    public DateTime EntryDate;
+}
+
+public class LeadCleaner : MonoBehaviour
+{
+
+    public List<Lead> DeDuplicateLeads(string jsonPath)
+    {
+        List<Lead> leads = new List<Lead>();
+
+
+        return leads;
+    }
+
+    private string LoadJsonFromDisk(string path)
+    {
+        try
+        {
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string json = sr.ReadToEnd();
+                return json;
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error reading file:");
+            Console.WriteLine(e.Message);
+        }
+
+        return null;
+    }
+
+    #region Tests
+
+    [Test]
+    public void LoadJsonFromDiskTest()
+    {
+        string json = LoadJsonFromDisk(Application.dataPath + "leads.json");
+        Assert.That(string.IsNullOrEmpty(json), Is.False);
+    }
+
+    #endregion
+
+}
+
---
+++ Assets/_Project/Tests/Editor/LeadCleanerTest.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class LeadCleanerTest {
+
+	[Test]
+	public void BasicTest()
+	{
+		bool testBool = false;
+		Assert.AreEqual(false, testBool);
+	}
+
+	[Test]
+	public void LoadJsonFromDiskTest()
+	{
+		bool testBool = false;
+		Assert.AreEqual(false, testBool);
+	}
+
+}
